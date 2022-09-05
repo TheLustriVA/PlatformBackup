3a0fad09c7c11a02e132fc777318aaa9ed309301
Marco Lustri <thelustriva@mgail.com>
2022-09-06 07:56:53 +1000

    Added 0Auth to Reddit using PRAW.

--- lib/api.py
+++ lib/api.py
@@ -1,4 +1,60 @@
 # This file will contain functions, handlers, and objects for connecting to various APIs and online services.
 
-# from dotenv import load_dotenv
-# import os
+from dotenv import load_dotenv
+import os
+import praw
+
+def get_refresh_token():
+    reddit = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_SECRET"),
+        refresh_token=os.getenv("REDDIT_REFRESH_TOKEN"),
+        user_agent="MonARCH-SI by /u/MediciVA"
+    )
+
+    print(reddit.auth.scopes)
+    
+    return reddit
+
+def get_reddit_client():
+    env_path = ".env"       # Set the path for the environment file
+    load_dotenv(dotenv_path=env_path)     # Load the environment file
+
+    reddit_temp = praw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_SECRET"),
+        redirect_url=os.getenv("REDDIT_REDIRECT"),
+        user_agent="MonARCH-SI by /u/MediciVA"
+    )
+
+    scopes_list = [
+        "account",
+        "edit",
+        "flair",
+        "history",
+        "identity",
+        "mysubreddits",
+        "privatemessages",
+        "read",
+        "save",
+        "submit",
+        "subscribe",
+        "vote",
+        "wikiread" 
+    ]
+
+    auth = reddit_temp.auth.url(scopes=scopes_list, state="MonARCH-SI", duration="permanent")
+    print(auth)
+    
+    auth_code = input("Enter the code from the URL: ")
+    refresh_token = reddit_temp.auth.authorize(auth_code)
+    redditor = reddit_temp.user.me()
+    print(refresh_token)
+    print(redditor)
+    
+    with open(".env", "a", encoding="utf-8") as f:
+        f.write(f"REDDIT_REFRESH_TOKEN={refresh_token}")
+
+    reddit = get_refresh_token()
+    
+    return reddit
